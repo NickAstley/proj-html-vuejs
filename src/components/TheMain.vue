2bd87a9114601ff1e5106a4d94fa9c9dba5103e9
NickAstley <andreanicastro1994@gmail.com>
2022-07-04 18:17:24 +0200

    Completed second section

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,82 +1,33 @@
 <template>
     <main>
         <div class="container">
-            <section class="work-culture">
-                <div class="row justify-content-around">
-                    <div class="col-4">
-                        <div class="text-uppercase mb-3">Everything in <span class="fw-semibold">MaxCoach</span></div>
-                        <h2 class="display-5 fw-semibold mb-3">Learn about our <span class="text-primary fw-light">Work Culture</span> at MaxCoach</h2>
-                        <p class="small-text mb-4">Spend some time to visit our website or head office and discover our current courses, enrollment procedure, and registration deadline. We're opening new classes every beginning of each month.</p>
-                        <span>Have questions? </span>
-                        <a href="#" class="text-primary fw-semibold">Get Free Guide <i class="fa-solid fa-arrow-right-long"></i></a>
-                    </div>
-                    <div class="col-6 floating-imgs-container">
-                        <img src="../../public/img/stock-full-hd-10.jpg" alt="Teamwork">
-                        <div class="random-circle"></div>
-                        <div class="random-dots">
-                            <img src="../../public/img/maxcoach-shape-07.png" alt="">
-                        </div>
-                        <div class="girl-working-img">
-                            <img class="rounded" src="../../public/img/about-us.jpg" alt="Ragazza al pc">
-                        </div>
-                        <div class="spinning-random-circles">
-                            <img src="../../public/img/maxcoach-shape-02.png" alt="">
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <!-- Sezione Work Culture -->
+            <WorkCulture/>
+            <UpgradeLife/>
         </div>
     </main>
 </template>
 
 <script>
-    export default {
+    import WorkCulture from './main/WorkCulture';
+    import UpgradeLife from './main/UpgradeLife';
 
+    export default {
+        components: {
+            WorkCulture,
+            UpgradeLife
+        }
     }
 </script>
 
 <style lang="scss">
     main {
-        padding-top: 10rem;
+        padding-top: 5rem;
         padding-bottom: 5rem;
 
-        .work-culture {
-            a {
-                border-bottom: 1px solid inherit;
-            }
-            p {
-                line-height: 1.5rem;
-            }
-            .floating-imgs-container{
-                position: relative;
-
-                .random-circle {
-                    position: absolute;
-                    top: -1rem;
-                    left: -6rem;
-                    width: 4rem;
-                    height: 4rem;
-                    border-radius: 50%;
-                    border: 0.5rem solid #ECC5AB;
-                }
-                .random-dots {
-                    position: absolute;
-                    top: -4rem;
-                    right: 5rem;
-                    z-index: -1;
-                }
-                .girl-working-img {
-                    position: absolute;
-                    top: 0;
-                    right: 0;
-                    transform: translate(0, -50%);
-                }
-                .spinning-random-circles {
-                    position: absolute;
-                    bottom: -6rem;
-                    right: 20%;
-                }
-            }
+        section {
+            padding-top: 5rem;
+            padding-bottom: 5rem;
         }
     }
 </style>
---
+++ src/components/main/UpgradeLife.vue
@@ -0,0 +1,78 @@
+<template>
+    <section class="upgrade-life">
+        <div class="row justify-content-around">
+            <!-- Immagine della ragazza con tutte le immagini svolazzanti -->
+            <div class="col-6 floating-imgs-container">
+                <!-- Immagine della ragazza che prende appunti -->
+                <img class="rounded" src="../../../public/img/stock-full-hd-11.jpg" alt="">
+                <!-- Logo di YouTube -->
+                <div class="youtube-logo">
+                    <img src="../../../public/img/icon-youtube-play.png" alt="">
+                </div>
+                <!-- Puntini in cerchio -->
+                <div class="random-dots-circle">
+                    <img src="../../../public/img/maxcoach-shape-05.png" alt="">
+                </div>
+                <!-- Cerchio random sulla destra -->
+                <div class="random-circle"></div>
+                <!-- Puntini in basso a destra -->
+                <div class="random-dots">
+                    <img src="../../../public/img/maxcoach-shape-07.png" alt="">
+                </div>
+            </div>
+            <!-- Blocco di testo "Upgrade yours skills, upgrade your life" sulla destraa -->
+            <div class="col-4 pt-5">
+                <div class="text-uppercase mb-3">How we work</div>
+                <h2 class="display-5 fw-semibold mb-3">Upgrade Your Skills <span class="text-primary fw-light">Upgrade Your Life</span></h2>
+                <p class="small-text mb-4">Opening up more opportunities in front of you to get the ticket to more enjoyable door in life. Getting equipped with new skills for new sets of insights into your career.<br>MaxCoach accompany learners in every stage of learning.</p>
+                <a href="#" class="fw-semibold">Download free guidebook <i class="fa-solid fa-arrow-right-long"></i></a>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .upgrade-life {
+
+        p {
+            line-height: 1.5rem;
+        }
+        .floating-imgs-container {
+            position: relative;
+
+            .youtube-logo {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(calc(-50% + 22px), -50%);
+            }
+            .random-dots-circle {
+                position: absolute;
+                top: 3rem;
+                left: -6rem;
+                z-index: -1;
+            }
+            .random-circle {
+                position: absolute;
+                top: -2rem;
+                right: -11rem;
+                width: 4rem;
+                height: 4rem;
+                border-radius: 50%;
+                border: 0.5rem solid #ECC5AB;
+            }
+            .random-dots {
+                position: absolute;
+                bottom: -6rem;
+                right: -2rem;
+                z-index: -1;
+            }
+        }
+    }
+</style>
