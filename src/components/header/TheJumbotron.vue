4f6a80b6ad7f2c411adaed3f5cb04bd83e75e140
NickAstley <andreanicastro1994@gmail.com>
2022-07-05 16:12:47 +0200

    Made changes to the Navbar so that it has position:fixed

--- src/components/header/TheJumbotron.vue
+++ src/components/header/TheJumbotron.vue
@@ -1,36 +1,38 @@
 <template>
-    <div class="jumbotron row align-items-center g-0">
-        <!-- Contenuto principale -->
-        <div class="text-white text-center">
-            <h2 class="fw-bold">Commence Business</h2>
-            <h1 class="display-3 mb-4">Distant Mentoring Program</h1>
-            <button class="btn btn-secondary">Download free guidebook <i class="fa-solid fa-arrow-right-long"></i></button>
-        </div>
-        <!-- Barra laterale con pulsanti -->
-        <div class="side-menu bg-white text-center rounded-start">
-            <!-- Icona squadra -->
-            <div>
-                <a href="#" class="clickable">
-                    <i class="fa-solid fa-ruler-horizontal"></i>
-                </a>
+    <div class="jumbotron">
+        <div class="row align-items-center h-100 g-0">
+            <!-- Contenuto principale -->
+            <div class="text-white text-center">
+                <h2 class="fw-bold">Commence Business</h2>
+                <h1 class="display-3 mb-4">Distant Mentoring Program</h1>
+                <button class="btn btn-secondary">Download free guidebook <i class="fa-solid fa-arrow-right-long"></i></button>
             </div>
-            <!-- Icona salvagente -->
-            <div>
-                <a href="#" class="clickable">
-                    <i class="fa-regular fa-life-ring"></i>
-                </a>
-            </div>
-            <!-- Icona libro -->
-            <div>
-                <a href="#" class="clickable">
-                    <i class="fa-solid fa-book"></i>
-                </a>
-            </div>
-            <!-- Icona carrello -->
-            <div>
-                <a href="#" class="clickable">
-                    <i class="fa-solid fa-cart-shopping"></i>
-                </a>
+            <!-- Barra laterale con pulsanti -->
+            <div class="side-menu bg-white text-center rounded-start">
+                <!-- Icona squadra -->
+                <div>
+                    <a href="#" class="clickable">
+                        <i class="fa-solid fa-ruler-horizontal"></i>
+                    </a>
+                </div>
+                <!-- Icona salvagente -->
+                <div>
+                    <a href="#" class="clickable">
+                        <i class="fa-regular fa-life-ring"></i>
+                    </a>
+                </div>
+                <!-- Icona libro -->
+                <div>
+                    <a href="#" class="clickable">
+                        <i class="fa-solid fa-book"></i>
+                    </a>
+                </div>
+                <!-- Icona carrello -->
+                <div>
+                    <a href="#" class="clickable">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -46,6 +48,7 @@
     @import '../../assets/scss/variables';
 
     .jumbotron {
+        margin-top: 74px;
         height: 700px;
         background-image: url("../../../public/img/hero.jpg");
         background-size: cover;
--- src/components/header/TheNavbar.vue
+++ src/components/header/TheNavbar.vue
@@ -1,43 +1,41 @@
 <template>
-    <div class="container-fluid">
-        <nav class="row justify-content-evenly align-items-center">
-            <!-- Componente che gestisce i dropdown -->
-            <NavDropdowns class="col-4 text-end mb-0" :dropdowns-list="dropdownsList"/>
-            <!-- Logo -->
-            <div class="logo-container col-2 text-center">
-                <a href="#" class="clickable">
-                    <img class="logo" src="img/logo.png" alt="Logo MaxCoach">
-                </a>
-            </div>
-            <!-- Menu di ricerca, accesso al carrello e al profilo -->
-            <div class="nav-menu col-4">
-                <div class="row justify-content-center align-items-center gx-0">
-                    <div class="col-auto">
-                        <!-- Icona del carello con numero di elementi in absolute -->
-                        <a href="#" class="clickable">
-                            <div class="position-relative">
-                                <i class="fa-solid fa-cart-shopping"></i>
-                                <span class="position-absolute top-0 start-100 translate-middle badge rounded-circle bg-primary">0</span>
-                            </div>
-                        </a>
-                        <!-- Icona del profilo -->
-                        <a class="clickable" href="#">
-                            <i class="fa-regular fa-circle-user"></i>
-                        </a>
-                    </div>
-                    <!-- Barra di ricerca -->
-                    <div class="col-6">
-                        <div class="search-input">
-                            <input class="form-control my-input small-text" type="text" placeholder="Search...">
-                            <span class="input-search-icon">
-                                    <i class="fa fa-search input-search-icon"></i>
-                            </span>
+    <nav class="row justify-content-evenly align-items-center">
+        <!-- Componente che gestisce i dropdown -->
+        <NavDropdowns class="col-4 text-end mb-0" :dropdowns-list="dropdownsList"/>
+        <!-- Logo -->
+        <div class="logo-container col-2 text-center">
+            <a href="#" class="clickable">
+                <img class="logo" src="img/logo.png" alt="Logo MaxCoach">
+            </a>
+        </div>
+        <!-- Menu di ricerca, accesso al carrello e al profilo -->
+        <div class="nav-menu col-4">
+            <div class="row justify-content-center align-items-center gx-0">
+                <div class="col-auto">
+                    <!-- Icona del carello con numero di elementi in absolute -->
+                    <a href="#" class="clickable">
+                        <div class="position-relative">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-circle bg-primary">0</span>
                         </div>
+                    </a>
+                    <!-- Icona del profilo -->
+                    <a class="clickable" href="#">
+                        <i class="fa-regular fa-circle-user"></i>
+                    </a>
+                </div>
+                <!-- Barra di ricerca -->
+                <div class="col-6">
+                    <div class="search-input">
+                        <input class="form-control my-input small-text" type="text" placeholder="Search...">
+                        <span class="input-search-icon">
+                                <i class="fa fa-search input-search-icon"></i>
+                        </span>
                     </div>
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -67,6 +65,13 @@
     @import '../../assets/scss/variables';
 
     nav {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 99;
+        background: white;
+        box-shadow: 0px 1px 10px #999;
 
         .logo-container {
 
